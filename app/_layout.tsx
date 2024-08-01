64fa75b3ff0b5d9ef3fbe52b757fc3ebe7cb4d2b
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-08-01 22:34:13 +0200

    Install Nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,11 @@
-import { Stack } from "expo-router";
+import { Slot, Stack } from "expo-router";
+import { Text } from "react-native";
 
 export default function RootLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
     </Stack>
+    
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,13 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View className="flex-1 justify-center items-center bg-slate-300">
+      <Text className="text-2xl">Aora !</Text>
+      <StatusBar style="auto"/>
+      <Link href="/profile" style={{ color: 'blue'}}>Go to profile</Link>
     </View>
   );
 }
