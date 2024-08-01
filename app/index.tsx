5ffafc7a8b1877f7c08511f93f077d745f52b885
JoesephEgbenchong <egbenchongjosephayuk@gmail.com>
2024-08-02 01:05:52 +0200

    configure themes and added .d.ts for react imports

--- app/index.tsx
+++ app/index.tsx
@@ -4,10 +4,10 @@ import { Text, View } from "react-native";
 
 export default function Index() {
   return (
-    <View className="flex-1 justify-center items-center bg-slate-300">
-      <Text className="text-2xl">Aora !</Text>
+    <View className="flex-1 justify-center items-center bg-white">
+      <Text className="text-2xl font-pblack">Aora !</Text>
       <StatusBar style="auto"/>
-      <Link href="/profile" style={{ color: 'blue'}}>Go to profile</Link>
+      <Link href="/profile" style={{ color: 'blue'}} className="font-plight">Go to profile</Link>
     </View>
   );
 }
